8f0a72399a79463c64db61de2072377961cbbaf0
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-03 23:51:49 +0100

    Exercise 1.4: course information step4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,46 +11,46 @@ const Part = (props) => (
 const Content = (props) => (
   <>
     <Part
-      part={props.part1}
+      part={props.parts[0]}
     />
     <Part
-      part={props.part2}
+      part={props.parts[1]}
     />
     <Part
-      part={props.part3}
+      part={props.parts[2]}
     />
   </>
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.exercises.reduce((a, b) => a + b, 0)}</p>
+  <p>Number of exercises {props.parts.map(p => p.exercises).reduce((a, b) => a + b, 0)}</p>
 )
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
+        parts={parts}
       />
       <Total
-        exercises={[part1.exercises, part2.exercises, part3.exercises]}
+        parts={parts}
       />
     </div>
   )
