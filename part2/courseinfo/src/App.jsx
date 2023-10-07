0a453298f97aa27cf183c76677358810ad268708
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-07 23:29:31 +0100

    Exercise 2.5: separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,46 +1,4 @@
-const Header = ({name}) => (
-  <h1>{name}</h1>
-)
-
-const Part = ({name, exercises}) => (
-  <p>
-    {name} {exercises}
-  </p>
-)
-
-const Content = ({parts}) => (
-  <>
-    {parts.map(part =>
-      <Part
-        key={part.id}
-        name={part.name}
-        exercises={part.exercises}
-      />
-    )}
-  </>
-)
-
-const Total = ({parts}) => (
-  <p>
-    <strong>
-      Total of {parts.map(p => p.exercises).reduce((a, b) => a + b, 0)} exercises
-    </strong>
-  </p>
-)
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content
-        parts={course.parts}
-      />
-      <Total
-        parts={course.parts}
-      />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
