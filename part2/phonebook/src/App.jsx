204623701c5793e7a46c98f9e27deaa82434f063
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-14 00:21:22 +0100

    Exercise 2.8: The Phonebook Step3: allow adding numbers to the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,32 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '08012345678'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewName = (event) => {
     event.preventDefault()
+    if (newName === '') {
+      alert('Please enter a name!')
+      return
+    }
+    if (newNumber === '') {
+      alert('Please enter a number!')
+      return
+    }
     if (persons.filter(person => person.name === newName).length > 0) {
       alert(`${newName} is already added to the phonebook`)
       return
     }
     setPersons(persons.concat(
       {
-        name: newName
+        name: newName,
+        number: newNumber
       }
     ))
   }
@@ -27,13 +40,16 @@ const App = () => {
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit" onClick={addNewName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
         (<div key={person.name}>
-          {person.name}
+          {person.name} {person.number}
         </div>)
       )}
     </div>
